226b72a2452ce47c7724f74647ac8f9fc10b9b28
jinho-wannagohome <jinho.jang@wesang.com>
2023-10-30 18:44:54 +0900

    add spm platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "FlexLayout",
+  platforms: [
+    .iOS(.v11)
+  ],
   products: [
     .library(name: "FlexLayout", targets: ["FlexLayout"]),
     .library(name: "FlexLayoutYoga", targets: ["FlexLayoutYoga"]),
